462c5d04bc08a6154fd6f68aecef9174929a7b4e
islam <56175059+Islam260301@users.noreply.github.com>
2023-11-21 18:52:01 +0600

    Add files via upload

---
+++ app.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <ui-button />
+    {{ $t('hello') }}
+    <button
+      v-for="l in availableLocales"
+      :key="l.code"
+      class="bg_primary"
+      @click="setLocale(l.code)"
+    >
+      {{ l.code }}
+    </button>
+    <nuxt-page />
+  </div>
+</template>
+
+<script lang="ts" setup>
+const { locale, locales, setLocale } = useI18n();
+
+const availableLocales = computed(() => {
+  return locales.value.filter(i => i.code !== locale.value);
+});
+</script>
+<style lang="scss"></style>
